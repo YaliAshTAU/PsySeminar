2f3feb7a78428212f484d089230a5ee0fbdf2a2b
yaliash <yaliash95@gmail.com>
2024-02-22 12:49:14 +0200

    scene embedded

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+import uuid
+from Video import Video
+
+if __name__ == "__main__":
+    the_office = Video("You're so white - The Office US.mp4")
+    the_office.print_scenes()
+    the_office.print_frames()
