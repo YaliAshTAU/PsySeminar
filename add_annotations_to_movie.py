a81874dc01395ebe4643f527cf5ebdf5f383ebd5
Yali Ash <yahelash@mail.tau.ac.il>
2024-11-08 16:05:42 +0200

    added Sherlock

--- add_annotations_to_movie.py
+++ add_annotations_to_movie.py
@@ -26,28 +26,34 @@ def add_relevant_annotation(all_annotations, count_annotation_frames):
 
 def add_text_to_movie(movie_path, new_path, all_annotations):
     cap = cv2.VideoCapture(movie_path)
+    if not cap.isOpened():
+        print(f"Error: Cannot open video file {movie_path}")
+        exit()
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = 25  #taken from https://www.nature.com/articles/s41597-020-00680-2. To get actual fps: fps= cap.get(cv2.CAP_PROP_FPS)
+    fps = 25  #for summer-25 taken from https://www.nature.com/articles/s41597-020-00680-2. To get actual fps: fps= cap.get(cv2.CAP_PROP_FPS), for sherlock-24
     out_cap = cv2.VideoWriter(new_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
 
     count_seconds = 0
     count_annotation_frames = 0
-    start_second = 40 # skip start credits
-    start_frame = start_second * fps
+    end_second = 3264 # skip start credits
+    end_frame = end_second * fps
     frame_count = 0
 
     while cap.isOpened():
         ret, frame = cap.read()
+        if not ret:
+            print("Error: Could not read frame")
+            break
         frame_count += 1
         if not ret:
             break
-        if frame_count < start_frame:
-            continue # don't save frames before start_second
+        if frame_count > end_frame:
+            continue # don't save frames after end_second
 
         count_seconds += 1 / fps
         # every 3 seconds, increase the annotation frame
-        if count_seconds >= 3:
+        if count_seconds >= 1.5:
             count_annotation_frames += 1
             count_seconds = 0
 
@@ -75,14 +81,17 @@ if __name__ == '__main__':
     new_path = args.new_path
 
     faces_annotations = get_annotations(annotation_dir, "face")
+    print("Faces annotations loaded:", faces_annotations is not None)
     written_text_annotations = get_annotations(annotation_dir, "written_text")
-    socia_noosocial_annotations = get_annotations(annotation_dir, "social_nonsocial")
+    print("Written text annotations loaded:", written_text_annotations is not None)
+    social_noosocial_annotations = get_annotations(annotation_dir, "social_nonsocial")
+    print("Social non-social annotations loaded:", social_noosocial_annotations is not None)
     speaking_annotations = get_annotations(annotation_dir, "speaking")
-    touch_annotations = get_annotations(annotation_dir, "touch")
+    print("Speaking annotations loaded:", speaking_annotations is not None)
+    # touch_annotations = get_annotations(annotation_dir, "touch")
     all_annotations = {"faces": faces_annotations,
                        "written_text": written_text_annotations,
-                       "social": socia_noosocial_annotations,
-                       "speaking": speaking_annotations,
-                       "touch": touch_annotations}
+                       "social": social_noosocial_annotations,
+                       "speaking": speaking_annotations}
 
     add_text_to_movie(movie_path, new_path, all_annotations)
